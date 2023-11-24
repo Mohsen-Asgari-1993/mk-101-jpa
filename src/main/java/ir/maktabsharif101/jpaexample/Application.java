62db30f7f5a085b3f02179bc85d7dddfad301904
maxixking66 <maxixking66@gamil.com>
2023-11-24 10:26:34 +0330

    add role and permissions to customers

--- src/main/java/ir/maktabsharif101/jpaexample/Application.java
+++ src/main/java/ir/maktabsharif101/jpaexample/Application.java
@@ -1,30 +1,55 @@
 package ir.maktabsharif101.jpaexample;
 
 import com.github.javafaker.Faker;
-import ir.maktabsharif101.jpaexample.domain.Wallet;
+import ir.maktabsharif101.jpaexample.domain.Customer;
+import ir.maktabsharif101.jpaexample.domain.Permission;
+import ir.maktabsharif101.jpaexample.domain.Role;
 import ir.maktabsharif101.jpaexample.service.CustomerService;
 import ir.maktabsharif101.jpaexample.service.dto.CustomerRegistrationDTO;
 import ir.maktabsharif101.jpaexample.util.ApplicationContext;
+import ir.maktabsharif101.jpaexample.util.TransactionProvider;
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
 
     public static void main(String[] args) {
 
-//        createFakeCustomers();
 
-        List<Wallet> all = ApplicationContext.getWalletRepository()
-                .findAll();
-        all.forEach(wallet ->
-                System.out.println(
-                        "walletId: " + wallet.getId() +
-                                " - customerId: " + wallet.getCustomer().getId() +
-                                " - customer firstName: " + wallet.getCustomer().getFirstName()
+    }
+
+    private static void addRolesAndPermissionsToCustomers() {
+        TransactionProvider.beginTransaction("init");
+
+        Permission permission = ApplicationContext.getPermissionService().save(
+                new Permission("customer")
+        );
+        Set<Permission> permissions = new HashSet<>();
+        permissions.add(permission);
+        Role customerRole = ApplicationContext.getRoleRepository().save(
+                new Role(
+                        "CUSTOMER",
+                        permissions
                 )
         );
 
+        List<Customer> customers = ApplicationContext.getCustomerRepository()
+                .findAll();
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(customerRole);
+
+        customers.forEach(customer -> {
+            customer.setRoles(
+                    roles
+            );
+            ApplicationContext.getCustomerRepository().save(customer);
+        });
+
+        TransactionProvider.commitTransaction("init");
     }
 
     private static void createFakeCustomers() {
