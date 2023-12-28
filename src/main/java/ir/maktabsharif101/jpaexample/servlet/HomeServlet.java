f37c86b92ea5c1e1b6adfb40245c7e5ddd237f0d
maxixking66 <maxixking66@gamil.com>
2023-12-28 16:33:33 +0330

    show jsp

--- src/main/java/ir/maktabsharif101/jpaexample/servlet/HomeServlet.java
+++ src/main/java/ir/maktabsharif101/jpaexample/servlet/HomeServlet.java
@@ -1,27 +1,25 @@
 package ir.maktabsharif101.jpaexample.servlet;
 
+import com.github.javafaker.Faker;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class HomeServlet extends HttpServlet {
 
+    private final Faker faker = new Faker();
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.write(
-                """
-                        <html lang="en">
-                        <body>
-                        <h1>This is home page</h1>
-                        <p>this is p tag</p>
-                        </body>
-                        </html>
-                            """
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute(
+                "myName", faker.name().fullName()
         );
+
+        request.getRequestDispatcher("home.jsp")
+                .forward(request, response);
     }
 }
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mohsen
+  Date: 12/28/2023
+  Time: 16:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+<h1 style="color: red">This is home page</h1>
+<p>hello from ${myName}</p>
+</body>
+</html>
